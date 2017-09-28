fcb5c6f66785f0a43ca5523361c20f49c35eb6b5
Pramod <pramod.ijjarothu@bennett.edu.in>
2017-09-28 08:57:04 +0530

    first

---
+++ JUnitTestExample/src/JUnitTesting.java
@@ -0,0 +1,18 @@
+
+
+
+
+public class JUnitTesting {
+
+public int addNumbers(int a ,int b) {
+	return a+b;
+}
+
+public String addStrings(String a,String b) {
+	String c= a+b;
+	return c;
+}
+
+
+
+}
